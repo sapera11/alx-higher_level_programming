ecf1a02074c9999b8131ac9020868a7eb422a084
sapera11 <akinsetewilliams@gmail.com>
2023-08-15 13:37:25 -0700

    updated

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,5 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
-
+-- insert a new row in table of given database
+-- database passed as argument
+--     id = 89
+--     name = Best school
+INSERT INTO `first_table` (`id`, `name`) VALUES(89, 'Best School');
